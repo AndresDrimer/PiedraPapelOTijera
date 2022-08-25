5bd5ae9e632eb85e802689c63f0d84f2d53e2dcd
AndresDrimer <andresdrimer@hotmail.com>
2022-08-25 14:36:19 -0300

    v1 para celu ok, falta responsive desktop y tablet

--- script.js
+++ script.js
@@ -6,34 +6,89 @@ const urlCartas = ["img/stone.jpg", "img/paper.jpg", "img/scissor3.jpg"]
 
 let cuadrado1 = document.querySelector("#cuadrado1")
 let cuadrado2 = document.querySelector("#cuadrado2")
-// cuadrado1.style.backgroundImage="url`${urlCartas[1]}`"
 
 // escuchar al boton y disparar funcion
 let boton=document.querySelector("#botonJugar")
 boton.addEventListener("click", jugar)
 
+// variables declaradas
 let contadorJugador = 0
 let contadorMaquina = 0
+let nombreIngresado = prompt("ingresa tu nombre: ")
 
-// let puntajeJugador = document.querySelector("#puntJugador")
-// puntajeJugador.appendChild("contadorJugador")
+
+function nombrar(){
+  let cargaNombre = document.createElement("p")
+  let nombre = document.createTextNode(nombreIngresado)
+  cargaNombre.appendChild(nombre)
+  document.querySelector("#nombreJugador").appendChild(cargaNombre)
+}
+
+function nombrarPuntaje(){
+let cargaNombrePuntaje =document.createElement("p")
+let nombreIngresadoDosPuntos = nombreIngresado + ": "
+let nombrePje = document.createTextNode(nombreIngresadoDosPuntos)
+cargaNombrePuntaje.appendChild(nombrePje)
+document.querySelector("#nombreJugadorPunt").appendChild(cargaNombrePuntaje)
+}
+
+function cargarPuntajes(){
+  
+  let cargaPuntajeJugador = document.createElement("p")
+  let puntJug = document.createTextNode(contadorJugador)
+  cargaPuntajeJugador.appendChild(puntJug)
+  document.querySelector("#puntJugador").appendChild(cargaPuntajeJugador)
+
+  
+  let cargaPuntajeMaquina = document.createElement("p")
+  let puntMaq = document.createTextNode(contadorMaquina)
+  cargaPuntajeMaquina.appendChild(puntMaq)
+  document.querySelector("#puntMaquina").appendChild(cargaPuntajeMaquina)
+
+}
 
 function ganarChico(){
   contadorJugador++
+  cargarPuntajes()
   console.log("ganaste, tenes " + contadorJugador + " puntos")
 }
 function perderChico(){
   contadorMaquina++
+  cargarPuntajes()
   console.log("ganó la Maquina, tiene  " + contadorMaquina + " puntos")
 }
 function empatarChico(){
   console.log("empate")
 }
 function ganarFinal(){
-  console.log("partida ganada!!")
+  let ganaste = document.createElement("h1")
+  let mensajeTriunfo = document.createTextNode("ganaste!!!")
+  ganaste.appendChild(mensajeTriunfo)
+  ganaste.setAttribute("style", "background:#90ee90");
+  document.querySelector("#pResultado").appendChild(ganaste)
+  jugarDeNuevo()
 }
-function perderFinal(){
-  console.log("partida perdida :(")
+function perderFinal(){ 
+  let perdiste = document.createElement("h1")
+  let mensajeDerrota = document.createTextNode("perdiste ;(")
+  perdiste.appendChild(mensajeDerrota)
+  perdiste.setAttribute("style", "background:#FF6666");
+  document.querySelector("#pResultado").appendChild(perdiste)
+  jugarDeNuevo()
+}
+
+function jugarDeNuevo() {
+  let deNuevo = document.createElement("button")
+  let jugar = document.createTextNode("NUEVA PARTIDA")
+  deNuevo.appendChild(jugar)
+  deNuevo.setAttribute("style", "background: #54D3EF");
+  document.querySelector("#jugarNuevamente").appendChild(deNuevo)
+  let boton=document.querySelector("#jugarNuevamente")
+  boton.addEventListener("click", recargar)
+
+}
+function recargar(){
+  window.location.reload()
 }
 // function nuevaPartida()
 
@@ -81,9 +136,8 @@ function jugar(){
     }else if (contadorMaquina == 3){
       perderFinal()
     }
-
-    // pasarle el puntaje a la tabla cada vez
-    
-
 }
+
+nombrar()
+nombrarPuntaje()
 
